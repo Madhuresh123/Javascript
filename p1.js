4ee22382f4acf12d361d1a171f0ae0c90f67d3f2
Madhuresh123 <madhuresh91394@gmail.com>
2023-07-21 10:45:42 +0530

    palindrom

---
+++ p1.js
@@ -0,0 +1,36 @@
+
+
+//reverse the word wisely
+
+// const word = "Hello World";
+
+// const wholereverse = word.split("").reverse().join("");
+
+// const final = wholereverse.split(" ").reverse().join(" ");
+
+// console.log(final);
+
+// reverse a string
+
+// const str = "Geeks of code";
+
+// const reversestr = str.split("").reverse().join("");
+
+// const finalreverse = reversestr.split(" ").reverse().join(" ");
+
+// console.log(finalreverse);
+
+
+//given word is plaindrom or not?
+
+// const word = "abbbc";
+
+// const revword = word.split("").reverse().join("");
+
+
+//     if(word === revword)
+//     console.log(true);
+//     else
+//     console.log(false);
+
+
